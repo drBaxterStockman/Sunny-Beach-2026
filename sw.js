4e06c24e37814eb5e407a46c8ad7872ba695554d
drBaxterStockman <s57rkw8b9n@privaterelay.appleid.com>
2026-08-26 17:17:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,47 @@
-const CACHE='sunny-beach-2026-v1';
-const ASSETS=['./','./index.html','./plan-putovanja.png','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+const CACHE='sunny-beach-2026-v2';
+const ASSETS=[
+  './',
+  './index.html',
+  './plan-putovanja.png',
+  './apple-touch-icon.png',
+  './icon-192.png',
+  './icon-512.png',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys
+          .filter(key => key.startsWith('sunny-beach-2026-') && key !== CACHE)
+          .map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
